04eba61eea7c4b6e073d87215c769439ff615023
Kseniya Naumycheva <naumycheva@naumycheva-lx.nixsolutions.com>
2019-07-04 18:49:14 +0300

    fourth fix with each_with_index

--- fourth.rb
+++ fourth.rb
@@ -8,8 +8,7 @@ def fetch_number_of_vowel(alphabet = {}, counter = 0)
   alphabet
 end
 
-def calculate_number_of_vowel(array_of_letters)
-  list = {}
+def calculate_number_of_vowel(array_of_letters,   list = {})
   array_of_letters.each_with_index { |letter, index| list[letter] = index}.select{|letter, _index| %w[a e i o u].include? letter}
 end
 
